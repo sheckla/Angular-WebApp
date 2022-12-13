717e874fc1eeb1c1385d7697d9cf120c68c2285b
danigraf <daniel.graf@hs-osnabrueck.de>
2022-12-13 10:29:03 +0100

    Project Cleanup

--- application/src/app/app-routing.module.ts
+++ application/src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DrawAppLayoutComponent } from './components/drawAppPage/draw-app-layout/draw-app-layout.component';
-import { IndexComponent } from './components/indexPage/index/index.component';
-import { QuizAppLayoutComponent } from './components/quizPage/quiz-app-layout/quiz-app-layout.component';
+import { IndexComponent } from './components/html/index/index.component';
+import { QuizAppLayoutComponent } from './components/html/apps/quiz-app/quiz-app-layout/quiz-app-layout.component';
 
 const routes: Routes = [
   { path: 'index', component: IndexComponent },
-  { path: 'drawApp', component: DrawAppLayoutComponent },
   { path: 'quizApp', component: QuizAppLayoutComponent}
 ];
 
--- application/src/app/app.module.ts
+++ application/src/app/app.module.ts
@@ -7,25 +7,17 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatSliderModule } from '@angular/material/slider';
-import { IndexComponent } from './components/indexPage/index/index.component';
-import { DrawAppLayoutComponent } from './components/drawAppPage/draw-app-layout/draw-app-layout.component';
-import { DrawAppCanvasComponent } from './components/drawAppPage/draw-app-canvas/draw-app-canvas.component';
-import { DrawAppToolsComponent } from './components/drawAppPage/draw-app-tools/draw-app-tools.component';
-import { UserLoginComponent } from './components/drawAppPage/user-login/user-login.component';
-import { DrawAppDashboardComponent } from './components/drawAppPage/draw-app-dashboard/draw-app-dashboard.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { QuizAppLayoutComponent } from './components/quizPage/quiz-app-layout/quiz-app-layout.component';
+import { IndexComponent } from './components/html/index/index.component';
+import { UserLoginComponent } from './components/userAuth/user-login/user-login.component';
+import { NavbarComponent } from './components/html/navbar/navbar.component';
+import { QuizAppLayoutComponent } from './components/html/apps/quiz-app/quiz-app-layout/quiz-app-layout.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DrawAppCanvasComponent,
     NavbarComponent,
     UserLoginComponent,
-    DrawAppToolsComponent,
-    DrawAppDashboardComponent,
-    DrawAppLayoutComponent,
     IndexComponent,
     QuizAppLayoutComponent,
   ],
