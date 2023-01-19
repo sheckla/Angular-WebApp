eb1ac11e38506a609f88259a642aa72a49ce443a
mihammer <mike.hammer@hs-osnabrueck.de>
2023-01-19 21:11:38 +0100

    dashboard first look

--- application/src/app/components/html/apps/quiz-app/quiz-dashboard/quiz-dashboard.component.ts
+++ application/src/app/components/html/apps/quiz-app/quiz-dashboard/quiz-dashboard.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { UserHandlerService } from 'src/app/components/userAuth/services/user-handler.service';
 
 @Component({
   selector: 'app-quiz-dashboard',
   templateUrl: './quiz-dashboard.component.html',
-  styleUrls: ['./quiz-dashboard.component.css']
+  styleUrls: ['./quiz-dashboard.component.css'],
 })
 export class QuizDashboardComponent implements OnInit {
+  lobbies: any = [];
 
-  constructor() { }
+  constructor(private userHandlerService: UserHandlerService) {
+    //TODO: Must be replaced with data from server
+    this.lobbies = [
+      {
+        name: 'Racewars',
+        users: 9,
+      },
 
-  ngOnInit(): void {
+      {
+        name: 'Basic',
+        users: 3,
+      },
+
+      {
+        name: 'Geography',
+        users: 9,
+      },
+      {
+        name: 'Skaters-Paradies',
+        users: 2,
+      },
+      {
+        name: 'Pornsections',
+        users: 10,
+      },
+      {
+        name: 'Series',
+        users: 1,
+      },
+    ];
   }
 
+  //username = this.userHandlerService.getUsername();
+  username = 'QuizMaster1239';
+
+  ngOnInit(): void {}
 }
