b662abc8e5ff56c64944b266c8a5f5f556b9fd96
mihammer <mike.hammer@hs-osnabrueck.de>
2023-01-19 10:59:56 +0100

    test comment on my own branch

--- application/src/app/components/userAuth/services/user-handler.service.ts
+++ application/src/app/components/userAuth/services/user-handler.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { io } from 'socket.io-client';
-
+//Test comment
 @Injectable({
   providedIn: 'root'
 })
