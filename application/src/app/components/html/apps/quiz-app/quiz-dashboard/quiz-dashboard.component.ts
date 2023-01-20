a987a497ca2c1398a8e873fa9e98cd82889c5c8e
mihammer <mike.hammer@hs-osnabrueck.de>
2023-01-20 16:05:44 +0100

    Filled Lobby for testing only

--- application/src/app/components/html/apps/quiz-app/quiz-dashboard/quiz-dashboard.component.ts
+++ application/src/app/components/html/apps/quiz-app/quiz-dashboard/quiz-dashboard.component.ts
@@ -11,75 +11,46 @@ import { Debug } from 'src/app/components/userAuth/services/util/Debug';
 })
 export class QuizDashboardComponent implements OnInit {
   private _subscriptions: Subscription[] = [];
-  public statusMessage: string = "";
+  public statusMessage: string = '';
   lobbies: any = [];
 
-  constructor(public userHandlerService: UserHandlerService,
-    private _errorMessageService: ErrorMessagesService) {
-
+  constructor(
+    public userHandlerService: UserHandlerService,
+    private _errorMessageService: ErrorMessagesService
+  ) {
     // Lobby Join Event
     this._subscriptions.push(
-      this.userHandlerService.lobbyJoinedEvent.subscribe(status => {
+      this.userHandlerService.lobbyJoinedEvent.subscribe((status) => {
         if (status) {
-          this.statusMessage = "";
+          this.statusMessage = '';
         } else {
           this.statusMessage = _errorMessageService.LobbyJoinError;
         }
       })
-    )
+    );
 
     // Lobby Creation Event
     this._subscriptions.push(
-      this.userHandlerService.lobbyCreationEvent.subscribe(status => {
+      this.userHandlerService.lobbyCreationEvent.subscribe((status) => {
         if (status) {
-          this.statusMessage = "";
+          this.statusMessage = '';
         } else {
           this.statusMessage = _errorMessageService.LobbyCreationError;
         }
       })
-    )
+    );
 
     // Fetch current Open Lobbies Event
     this._subscriptions.push(
-      this.userHandlerService.openLobbyFetchEvent.subscribe(lobbies => {
+      this.userHandlerService.openLobbyFetchEvent.subscribe((lobbies) => {
         this.lobbies = lobbies;
       })
-    )
-
-    //TODO: Must be replaced with data from server
-    this.lobbies = [
-      {
-        name: 'Racewars',
-        users: 9,
-      },
-
-      {
-        name: 'Basic',
-        users: 3,
-      },
-      {
-        name: 'Geography',
-        users: 9,
-      },
-      {
-        name: 'Skaters-Paradies',
-        users: 2,
-      },
-      {
-        name: 'Pornsections',
-        users: 10,
-      },
-      {
-        name: 'Series',
-        users: 1,
-      },
-
-    ];
+    );
   }
 
   ngOnDestroy(): void {
     // Unsubscribe when leaving component
-    this._subscriptions.forEach(subscription => subscription.unsubscribe());
+    this._subscriptions.forEach((subscription) => subscription.unsubscribe());
   }
 
   createLobby(name: string) {
@@ -96,14 +67,14 @@ export class QuizDashboardComponent implements OnInit {
     this.userHandlerService.logout();
   }
 
-  private validateLobbyName(lobbyName: string) : boolean {
+  private validateLobbyName(lobbyName: string): boolean {
     if (lobbyName.length < 4) {
       this.statusMessage = this._errorMessageService.LobbyNameTooShortError;
       return false;
     }
-    this.statusMessage = "";
+    this.statusMessage = '';
     return true;
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 }
--- application/src/app/components/userAuth/services/user-handler.service.ts
+++ application/src/app/components/userAuth/services/user-handler.service.ts
@@ -7,11 +7,11 @@ import { User, LobbyInfo } from './util/QuizAppDataTypes';
   providedIn: 'root',
 })
 /*
-* UserHandlerService:
-* - This service will be an connection-interface for the Client to Server communication
-* - Status Events will be provided by subscribeable EventEmitters
-* - Saves information regarding aspects of the quiz-game (User, Lobby, Questions ...)
-*/
+ * UserHandlerService:
+ * - This service will be an connection-interface for the Client to Server communication
+ * - Status Events will be provided by subscribeable EventEmitters
+ * - Saves information regarding aspects of the quiz-game (User, Lobby, Questions ...)
+ */
 export class UserHandlerService {
   private _clientConnection: ClientSocket = new ClientSocket();
   private _user: User = new User();
@@ -19,79 +19,93 @@ export class UserHandlerService {
   private _openLobbies: LobbyInfo[] = [];
 
   /*
-  * Subscribeable EventEmitters:
-  * - be notified when the client-socket receives an answer from the server
-  * - Event must be first emitted inside UserHanderService
-  *
-  * Example:
-  *   this.userHandlerService.loginEventStatus.subscribe(statusBoolean => {
-  *    // callback - do stuff here when Event happens
-  *    })
-  */
+   * Subscribeable EventEmitters:
+   * - be notified when the client-socket receives an answer from the server
+   * - Event must be first emitted inside UserHanderService
+   *
+   * Example:
+   *   this.userHandlerService.loginEventStatus.subscribe(statusBoolean => {
+   *    // callback - do stuff here when Event happens
+   *    })
+   */
   public lobbyJoinedEvent = new EventEmitter<boolean>();
   public lobbyCreationEvent = new EventEmitter<boolean>();
   public openLobbyFetchEvent = new EventEmitter<any[]>();
 
   constructor() {
+    // TODO: PLEASE REMOVE WHEN LOGIN SCREEN IS FINISHED
+    this.fillLobbyForTesting();
   }
 
   // Socket Listeners for Quiz-Game Client<->Server communication
   initQuizGameListeners() {
     // Login Status
-    this._clientConnection.getSocket().on("Client_SendUsername_Status", (status) => {
-      this._user.isLoggedIn = status;
-    })
+    this._clientConnection
+      .getSocket()
+      .on('Client_SendUsername_Status', (status) => {
+        this._user.isLoggedIn = status;
+      });
 
     // Current Open Lobbies
-    this._clientConnection.getSocket().on("CurrentOpenLobbies", (lobbies) => {
+    this._clientConnection.getSocket().on('CurrentOpenLobbies', (lobbies) => {
       var openLobbyInfo: any = [];
       for (var i = 0; i < lobbies.length; i++) {
         var lobby: LobbyInfo = this.parseLobbyInfo(lobbies[i]);
         var info = {
           name: lobby.name,
           users: lobby.users.length + 1, // (leader)
-        }
+        };
         openLobbyInfo.push(info);
       }
       this.openLobbyFetchEvent.emit(openLobbyInfo);
-    })
-
-    // Lobby Creation Status
-    this._clientConnection.getSocket().on("Client_LobbyCreationRequest_Status", (status) => {
-      this._user.isInsideLobby = status;
-      this.lobbyCreationEvent.emit(status);
     });
 
+    // Lobby Creation Status
+    this._clientConnection
+      .getSocket()
+      .on('Client_LobbyCreationRequest_Status', (status) => {
+        this._user.isInsideLobby = status;
+        this.lobbyCreationEvent.emit(status);
+      });
+
     // Lobby Join Status
-    this._clientConnection.getSocket().on("Client_LobbyJoinRequest_Status", (status) => {
-      this._user.isInsideLobby = status;
-      this.lobbyJoinedEvent.emit(status);
-    })
+    this._clientConnection
+      .getSocket()
+      .on('Client_LobbyJoinRequest_Status', (status) => {
+        this._user.isInsideLobby = status;
+        this.lobbyJoinedEvent.emit(status);
+      });
 
     // Lobby Leave Status
-    this._clientConnection.getSocket().on("Client_LobbyLeaveRequest_Status", (status) => {
-      if (status == true) {
-        this._user.isInsideLobby = false;
-      } else {
-        this._user.isInsideLobby = true;
-      }
-    })
+    this._clientConnection
+      .getSocket()
+      .on('Client_LobbyLeaveRequest_Status', (status) => {
+        if (status == true) {
+          this._user.isInsideLobby = false;
+        } else {
+          this._user.isInsideLobby = true;
+        }
+      });
 
     // Lobby Information Status
-    this._clientConnection.getSocket().on("CurrentLobbyInformationChanged", (info) => {
-      this._currentLobby = this.parseLobbyInfo(info);
-    })
+    this._clientConnection
+      .getSocket()
+      .on('CurrentLobbyInformationChanged', (info) => {
+        this._currentLobby = this.parseLobbyInfo(info);
+      });
 
     // Question Topic Status
-    this._clientConnection.getSocket().on("Client_RequestQuestionTopics_Status", (questions) => {
-      console.log(questions);
-    })
+    this._clientConnection
+      .getSocket()
+      .on('Client_RequestQuestionTopics_Status', (questions) => {
+        console.log(questions);
+      });
   }
 
   // *** Emit Events
   login(name: string) {
-    Debug.log("Username: " + name + " login-request sent to server");
-    this._clientConnection.getSocket().emit("Client_SendUsername", name);
+    Debug.log('Username: ' + name + ' login-request sent to server');
+    this._clientConnection.getSocket().emit('Client_SendUsername', name);
     this._user.name = name;
   }
 
@@ -102,22 +116,32 @@ export class UserHandlerService {
   }
 
   createLobby(lobbyName: string) {
-    this._clientConnection.getSocket().emit("Client_LobbyCreationRequest", lobbyName);
+    this._clientConnection
+      .getSocket()
+      .emit('Client_LobbyCreationRequest', lobbyName);
   }
 
   joinLobby(lobbyName: string) {
-    this._clientConnection.getSocket().emit("Client_LobbyJoinRequest", lobbyName);
+    this._clientConnection
+      .getSocket()
+      .emit('Client_LobbyJoinRequest', lobbyName);
   }
 
   leaveLobby(): void {
-    if (this._currentLobby.name == "") return;
-    Debug.log("Leaving Lobby");
-    this._clientConnection.getSocket().emit("Client_LobbyLeaveRequest", this._currentLobby.name);
+    if (this._currentLobby.name == '') return;
+    Debug.log('Leaving Lobby');
+    this._clientConnection
+      .getSocket()
+      .emit('Client_LobbyLeaveRequest', this._currentLobby.name);
   }
 
   startLobby(): void {
-    this._clientConnection.getSocket().emit("Client_LobbyStartRequest", this._currentLobby.name); //TODO
-    this._clientConnection.getSocket().emit("Client_RequestQuestionTopics", this._currentLobby.name);
+    this._clientConnection
+      .getSocket()
+      .emit('Client_LobbyStartRequest', this._currentLobby.name); //TODO
+    this._clientConnection
+      .getSocket()
+      .emit('Client_RequestQuestionTopics', this._currentLobby.name);
   }
 
   // *** Getters
@@ -149,5 +173,25 @@ export class UserHandlerService {
     this._user = new User();
     this._clientConnection = new ClientSocket();
     this._currentLobby = new LobbyInfo();
+  }
+
+  // *** FILL LOBBY FOR TESTING ONLY ***
+  // TODO: PLEASE REMOVE WHEN LOGIN SCREEN IS FINISHED
+  private fillLobbyForTesting() {
+    var testLobby = new LobbyInfo();
+    var testUser = new User();
+    //Filling half of User
+    testUser.name = 'Jeff';
+    //Filling the lobby
+    testLobby.name = 'Testos Teron Lobby';
+    testLobby.leader = 'Mike';
+    testLobby.users = ['mike', 'daniel', 'steve', 'max', 'liz22', 'heiko'];
+    testLobby.category = 'Art';
+    testLobby.totalQuestions = 10;
+    testLobby.difficulty = 'hard';
+    testLobby.started = false;
+    //Override
+    this._currentLobby = testLobby;
+    this._user = testUser;
   }
 }
